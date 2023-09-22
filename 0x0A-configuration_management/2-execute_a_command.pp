232d412f77878278fbb3e7e54b289e0bfad02e0b
emdinga <emdinga@gmail.com>
2023-09-22 12:30:40 -0700

    Puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,4 @@
-# Kill a process named "killmenow"
-exec { 'killmenow':
-  command => 'pkill -f killmenow',
-  onlyif  => 'pgrep -f killmenow',
+#Kills a process named killmenow
+exec{'pkill -f killmenow':
+path  => '/usr/bin/:/usr/local/bin/:/bin/'
 }
