ee37391fc7186daccbfd1eb5775867692824915f
emdinga <emdinga@gmail.com>
2023-09-22 12:28:07 -0700

    puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,4 @@
-# Puppet manifest to kill a process named "killmenow"
+# Kill a process named "killmenow"
 exec { 'killmenow':
   command => 'pkill -f killmenow',
   onlyif  => 'pgrep -f killmenow',
