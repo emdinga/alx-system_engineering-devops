1d749e177d2d471c56fd57eb8e60f6297eb00a1f
root <Emdinga>
2023-10-23 07:23:30 -0700

    Apii

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""Returns to-do list"""
-import requests
-import sys
+"""
+returns information about his/her TODO list progress
+"""
 
-def get_employee_todo_progress(employee_id):
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(employee_id)).json()
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+from requests import get
+from sys import argv
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    
-    total_tasks = len(todos)
-    completed_tasks = len(completed)
+if __name__ == '__main__':
+    main_url = 'https://jsonplaceholder.typicode.com'
+    todo_url = main_url + "/user/{}/todos".format(argv[1])
+    name_url = main_url + "/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), completed_tasks, total_tasks))
-
-    for c in completed:
-        print("\t{}".format(c))
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    get_employee_todo_progress(employee_id)
-
+    todo_num = len(todo_result)
+    todo_complete = len([todo for todo in todo_result
+                         if todo.get("completed")])
+    name = name_result.get("name")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, todo_complete, todo_num))
+    for todo in todo_result:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
