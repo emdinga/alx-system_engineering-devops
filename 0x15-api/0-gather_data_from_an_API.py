05e4288831ca374fce498ed6f5cd3884e9e68c80
root <Emdinga>
2023-10-23 07:27:04 -0700

    Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,43 @@
 #!/usr/bin/python3
 """
-returns information about his/her TODO list progress
+Retrieve employees list 
 """
 
-from requests import get
-from sys import argv
+import requests
+import sys
 
-if __name__ == '__main__':
+def get_employee_todo_progress(employee_id):
+    """
+    Retrieves and displays employee's TODO list progress.
+
+    Args:
+        employee_id (int): The employee ID.
+
+    Returns:
+        None
+    """
     main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = main_url + "/user/{}/todos".format(argv[1])
-    name_url = main_url + "/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
+    todo_url = f"{main_url}/todos?userId={employee_id}"
+    name_url = f"{main_url}/users/{employee_id}"
+    
+    todo_result = requests.get(todo_url).json()
+    name_result = requests.get(name_url).json()
 
     todo_num = len(todo_result)
-    todo_complete = len([todo for todo in todo_result
-                         if todo.get("completed")])
-    name = name_result.get("name")
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, todo_complete, todo_num))
+    todo_complete = sum(1 for todo in todo_result if todo["completed"])
+    name = name_result["name"]
+
+    print(f"Employee {name} is done with tasks({todo_complete}/{todo_num}):")
+    
     for todo in todo_result:
-        if (todo.get("completed")):
-            print("\t {}".format(todo.get("title")))
+        if todo["completed"]:
+            print(f"\t {todo['title']}")
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
+
